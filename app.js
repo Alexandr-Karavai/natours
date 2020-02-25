a73bd1a0919b8e6f2d4a70f87583c345d79463e3
Alexandr Karavai <karavai96-2010@mail.ru>
2020-02-25 17:17:39 +0300

    Add security best practices

--- app.js
+++ app.js
@@ -3,28 +3,53 @@ const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const sanitize = require('mongo-sanitize');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
-const app = express();
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
 
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, 'access.log'),
   { flags: 'a' }
 );
-
+// Limit requests from same API
 const limitReq = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api', limitReq);
-
+//Body parser
 app.use(express.json());
+//Data sanitize
+app.use(sanitize());
+app.use(xss());
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use(morgan('combined', { stream: accessLogStream }));
