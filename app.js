92fd1469421c280ab07bc16fc1a13746f1166a3f
Alexandr Karavai <karavai96-2010@mail.ru>
2020-02-25 16:44:02 +0300

    Add rate limit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -15,6 +16,13 @@ const accessLogStream = fs.createWriteStream(
   path.join(__dirname, 'access.log'),
   { flags: 'a' }
 );
+
+const limitReq = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limitReq);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
