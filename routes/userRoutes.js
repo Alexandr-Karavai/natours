7dd630c88d0b3e0def1dec2b456148beb570a87e
Alexandr <karavai96-2010@mail.ru>
2020-03-04 15:16:25 +0300

    Add updateMe router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.post('/signup', authController.signUp);
 router.post('/login', authController.logIn);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.post('/resetPassword/:token', authController.resetPassword);
 
@@ -14,6 +13,10 @@ router.use(authController.protect);
 
 router.get('/me', userController.getMe, userController.getUser);
 router.delete('/deleteMe', userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.patch('/updateMe', userController.updateMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
