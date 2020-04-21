4a101ed767e8429a9be30d063a90769dbaa52aeb
Alexandr Karavai <karavai96-2010@mail.ru>
2020-04-21 22:29:59 +0300

    Add update data user

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,12 @@
 import '@babel/polyfill';
 import { login, logOut } from './login';
+import { updateData } from './updateSettings';
 import { displayMap } from './mapbox';
 
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form--login');
+const updateForm = document.querySelector('.form-user-data');
+const updatePasswordForm = document.querySelector('.form-user-password');
 const logOutBtn = document.querySelector('.nav__el--logout');
 
 if (mapBox) {
@@ -23,3 +26,36 @@ if (loginForm) {
 }
 
 if (logOutBtn) logOutBtn.addEventListener('click', e => logOut());
+
+if (updateForm) {
+  updateForm.addEventListener('submit', e => {
+    e.preventDefault();
+    document.querySelector('.btn--save-settings').textContent = 'Updating...';
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    updateData({ name, email }, 'data');
+    document.querySelector('.btn--save-settings').textContent = 'Updating...';
+  });
+}
+
+if (updatePasswordForm) {
+  updatePasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+
+    await updateData(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
+}
