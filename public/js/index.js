f109602d903a1b4fcda4c822393e93ce623ed228
Alexandr Karavai <karavai96-2010@mail.ru>
2020-04-23 21:33:04 +0300

    Add booking

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,14 @@ import '@babel/polyfill';
 import { login, logOut } from './login';
 import { updateData } from './updateSettings';
 import { displayMap } from './mapbox';
+import { bookTour } from './stripe';
 
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const updateForm = document.querySelector('.form-user-data');
 const updatePasswordForm = document.querySelector('.form-user-password');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const bookBtn = document.getElementById('book-tour');
 
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
@@ -34,7 +36,6 @@ if (updateForm) {
     form.append('name', document.getElementById('name').value);
     form.append('email', document.getElementById('email').value);
     form.append('photo', document.getElementById('photo').files[0]);
-    console.log(form);
     updateData(form, 'data');
   });
 }
@@ -56,5 +57,13 @@ if (updatePasswordForm) {
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
+  });
+}
+
+if (bookBtn) {
+  bookBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
   });
 }
---
+++ public/js/stripe.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe('pk_test_GP52cD93PGcjxftcwqtdMoVw00PE4qcMxI');
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://localhost:5000/api/v1/bookings/checkout-session/${tourId}`
+    );
+
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    showAlert('error', err);
+  }
+};
